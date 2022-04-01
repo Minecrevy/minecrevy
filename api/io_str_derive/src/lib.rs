6d5aceb4959224daec02f176edad6b39430eb2c1
Christian Hughes <xdotdash@gmail.com>
2022-03-31 23:47:04 -0500

    minecrevy_io_str_derive 0.5.0: forgot `tag` attribute

--- api/io_str_derive/src/lib.rs
+++ api/io_str_derive/src/lib.rs
@@ -7,13 +7,13 @@ mod read;
 mod util;
 mod write;
 
-#[proc_macro_derive(McRead, attributes(io_repr, options))]
+#[proc_macro_derive(McRead, attributes(io_repr, options, tag))]
 pub fn McRead(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
     read::generate_read_impl(input).into()
 }
 
-#[proc_macro_derive(McWrite, attributes(io_repr, options))]
+#[proc_macro_derive(McWrite, attributes(io_repr, options, tag))]
 pub fn McWrite(input: proc_macro::TokenStream) -> proc_macro::TokenStream {
     let input = parse_macro_input!(input as DeriveInput);
 
