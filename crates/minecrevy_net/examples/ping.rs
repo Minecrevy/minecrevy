31ff15676573a8d6b66b0cf65f22c52a3b59a35f
Christian Hughes <xdotdash@gmail.com>
2023-04-10 20:02:16 -0500

    Use new `listen` system builder in example

--- crates/minecrevy_net/examples/ping.rs
+++ crates/minecrevy_net/examples/ping.rs
@@ -1,17 +1,14 @@
 use std::time::Duration;
 
 use bevy::{app::ScheduleRunnerSettings, prelude::*};
-use minecrevy_net::{
-    protocol::{packet::PacketsPlugin, plugin::DefaultNetworkPlugins, version::ReleaseVersion},
-    raw::RawServer,
+use minecrevy_net::protocol::{
+    packet::PacketsPlugin,
+    plugin::{listen, DefaultNetworkPlugins},
+    version::ReleaseVersion,
 };
 
 /// How often the server loop should tick.
 const TICKS_PER_SECOND: f64 = 20.0;
-
-fn start_listening(mut server: ResMut<RawServer>) {
-    server.listen("127.0.0.1:25565".parse().unwrap());
-}
 
 fn main() {
     let tick_duration = Duration::from_secs_f64(1.0 / TICKS_PER_SECOND);
@@ -21,6 +18,6 @@ fn main() {
         .add_plugins(MinimalPlugins)
         .add_plugin(PacketsPlugin::new([ReleaseVersion::V1_19_4.v()]))
         .add_plugins(DefaultNetworkPlugins)
-        .add_systems(PostStartup, start_listening)
+        .add_systems(PostStartup, listen("127.0.0.1:25565".parse().unwrap()))
         .run();
 }
